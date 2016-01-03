e5cb93214b5720faafaaedfdac7e690765e74b34
Karl Isenberg <karl@mesosphere.io>
2016-01-03 15:48:31 -0800

    Make install.sh not require git or make

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Install resolveip from the internet
+# Install resolveip from github
 # Usage: curl -o- https://raw.githubusercontent.com/karlkfi/resolveip/v1.0.1/install.sh | bash
 # Requires: make, git
 
@@ -8,12 +8,12 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-version=${1:-}
+prefix="/usr/local/bin"
 
-curl -o- https://raw.githubusercontent.com/karlkfi/intemp/v1.0.0/intemp.sh | bash -s -- -t 'intemp' "
-  version='${version:-}' &&
-  git clone https://github.com/karlkfi/resolveip . &&
-  [ -z \"\${version}\" ] && version=\$(git describe --abbrev=0 --tags);
-  git checkout \${version} &&
-  make install
-"
+version=${1:-}
+if [ -z "${version}" ]; then
+  version=$(curl -s https://api.github.com/repos/karlkfi/resolveip/releases/latest | grep 'tag_name' | cut -d\" -f4)
+fi
+
+curl -o- "https://raw.githubusercontent.com/karlkfi/resolveip/${version}/resolveip.sh" > "${prefix}/resolveip.sh"
+chmod a+x "${prefix}/resolveip.sh"
